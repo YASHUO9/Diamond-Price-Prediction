a9adba9e6edbc357635bc0592c1f10491318d9ab
YASHUO9 <jp985083@gmail.com>
2024-09-05 14:45:20 +0530

    info add

--- src/DimondPricePrediction/exception.py
+++ src/DimondPricePrediction/exception.py
@@ -3,6 +3,8 @@ import sys
 
 class customexception(Exception):
     def __init__(self,error_message,error_details:sys):
+        """ custom exception class to log error message, line number and file name"""
+        
         self.error_message = error_message
         _,_,exc_tb = error_details.exc_info()
         
@@ -10,6 +12,8 @@ class customexception(Exception):
         self.file_name=exc_tb.tb_frame.f_code.co_filename 
     
     def __str__(self):
+        """string representation of custom exception"""
+        
         return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
         self.file_name, self.lineno, str(self.error_message))
         
@@ -17,9 +21,12 @@ class customexception(Exception):
     
 
 if __name__ == "__main__":
+    
+    """ testing custom exception"""
     try:
         
         a=1/0
+        #zero division error
     
     except Exception as e:
         raise customexception(e,sys)
